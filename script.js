6345c91ae2390b1c13bca5b3fc1841ca320c5e62
TERRYYYY <kariuki.terry11@gmail.com>
2025-01-05 20:37:00 +0300

    Function that grabs whatever I type on the prompt area and prints it out to the console

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const typingForm = document.querySelector(".typing-form");
+
+let userMessage = null;
+
+const handleOutgoingChat = () => {
+    userMessage = typingForm.querySelector(".typing-input").value.trim();
+
+    if (!userMessage) return; //Exit if there is no message
+
+    console.log(userMessage);
+}
+
+//Prevent default form submission and handle ougoing chat
+typingForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // Prevent the page from refreshing and goes to the function below
+
+    handleOutgoingChat();
+});
