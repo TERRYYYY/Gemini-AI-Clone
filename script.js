4aa81c10797b43ad82194257c53ffc1e4e1b245c
TERRYYYY <kariuki.terry11@gmail.com>
2025-01-05 20:45:44 +0300

    Function that creates a new message element and returns it

--- script.js
+++ script.js
@@ -2,12 +2,27 @@ const typingForm = document.querySelector(".typing-form");
 
 let userMessage = null;
 
+//Create a new message element and return it
+const createMessageElement = (content, className) => {
+    const div = document.createElement ("div");
+    div.classList.add("message", className);
+    div.innerHTML = content;
+    return div;
+}
+
 const handleOutgoingChat = () => {
     userMessage = typingForm.querySelector(".typing-input").value.trim();
 
     if (!userMessage) return; //Exit if there is no message
 
     console.log(userMessage);
+
+    const html = `<div class="message-content">
+                    <img src="./images/gemini.svg" alt="Gemini Image" class="avatar">
+                    <p class="text"></p>
+                </div>`
+    
+    const outgoingMessageDiv = createMessageElement (html, "outgoing");
 }
 
 //Prevent default form submission and handle ougoing chat
